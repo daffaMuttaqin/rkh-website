7503945ceccfc8b7cddd5d819f7effd45e1ad6b9
Daffa Muttaqin <nine@upi.edu>
2024-02-26 19:44:11 +0700

    Menambahkan fitur dapatkan diskon 10%

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -118,7 +118,7 @@
                         <button data-modal-target="modal-tambah-point" data-modal-toggle="modal-tambah-point" type="button" class="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900">Tambah Poin</button>
 
                         <!-- TOMBOL DAPAT POINT -->
-                        <button type="button" class="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900">Dapatkan Diskon</button>
+                        <button data-modal-target="modal-dapatkan-diskon" data-modal-toggle="modal-dapatkan-diskon" type="button" class="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900">Dapatkan Diskon</button>
                     </div>
 
                     <!-- SEARCH JIKA ADA -->
@@ -458,6 +458,40 @@
 
 
     <!-- MODAL -->
+
+    <!-- MODAL DAPATKAN DISKON -->
+    <div id="modal-dapatkan-diskon" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative p-4 w-full max-w-md max-h-full">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <!-- Modal header -->
+                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                        Dapatkan Diskon 10%
+                    </h3>
+                    <button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="modal-dapatkan-diskon">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <!-- Modal body -->
+                <div class="p-4 md:p-5">
+                    <form class="space-y-4" action="<?= base_url('admin/dapatkan_diskon'); ?>" method="post">
+                        <!-- KODE AKUN -->
+                        <div>
+                            <label for="code_account" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kode Akun</label>
+                            <input type="text" name="code_account" id="code_account" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Masukkan Kode Akun Pengguna" required />
+                        </div>
+
+                        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Dapatkan</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END MODAL DAPATKAN DISKON -->
 
     <!-- MODAL TAMBAH POINT -->
     <div id="modal-tambah-point" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
